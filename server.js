71bd29f1717523315fc510e32edc9d8c98c7805c
Jathniel <Jathnielargueza@gmail.com>
2018-04-23 15:22:43 +0800

    create express app server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+'use strict';
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose  from 'mongoose';
+
+import router  from './config/routes';
+
+mongoose.connect('mongodb://localhost:27017/twitter');
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;
+
+
+app.use('/api', router);
+
+app.listen(port);
+console.log('Listening ' + port);
