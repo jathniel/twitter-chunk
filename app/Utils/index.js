1eaa14666ca93efdaa6d7346454401f845815a17
Jathniel <Jathnielargueza@gmail.com>
2018-05-07 23:22:46 +0800

    add counter for checking if tense or hundreds

--- app/Utils/index.js
+++ app/Utils/index.js
@@ -12,25 +12,31 @@ export default {
   }
 };
 const getSize = (messageLength) => {
-  let length = Math.round(messageLength/50).toString().length;
+  let length = numberCount(Math.round(messageLength/50));
   return 50 - (length + 2);
+};
+const numberCount = (number) => {
+  return number.toString().length;
 };
 const split = (message, InitialLength) => {
   const messageArray = [];
   let messageLeft = message;
   let length = InitialLength;
+  let count = 1;
   do {
 
     if(messageLeft.charAt(length) !== ' ' && messageLeft.length > InitialLength) {
       length--;
     } else {
       messageArray.push(messageLeft.substring(0, length).trim());
-      messageLeft = messageLeft.substring(length, messageLeft.length);
-      length = InitialLength;
+      messageLeft = messageLeft.substring(length + 1, messageLeft.length);
+      length = InitialLength  - (numberCount(count));
+      count++;
     }
     if(length === 0 && messageLeft.length > InitialLength) {
       throw 'unsplittable';
     }
+
   } while (messageLeft.length > 0);
 
   return messageArray;
