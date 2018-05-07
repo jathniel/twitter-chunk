7a623aaca90b2f4d603134cdc7ef4951f528676e
Jathniel <Jathnielargueza@gmail.com>
2018-05-07 20:59:53 +0800

    fix map series when adding to the database

--- app/controllers/TweetController.js
+++ app/controllers/TweetController.js
@@ -1,6 +1,7 @@
 import Tweet  from '../models/TweetModel';
 import  Utils from '../Utils';
-import forEach from 'lodash.foreach';
+import Promise from 'bluebird';
+
 export default {
   getTweets: (req, res) => {
     Tweet.find().sort({date: -1})
@@ -11,17 +12,16 @@ export default {
   },
   addTweet: (req, res) => {
     let data = Utils.splitMessage(req.body.title);
-    data = data.reverse();
-    forEach(data, (value) => {
+    data.reverse();
+    Promise.mapSeries(data, function(value) {
       const form = {
         title: value
       };
       const newTweet = new Tweet(form);
-      newTweet.save()
-      .catch((e) => res.json(e));
+      return newTweet.save();
+    }).then(function() {
+      return res.json('ok');
     });
-    return res.json('ok');
-
   },
   render: (req, res) => {
     res.render('index');
